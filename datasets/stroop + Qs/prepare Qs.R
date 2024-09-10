841715ab9cbaf6c94d1ca7fc374b2d90e386eeac
BoazRosenberg <133665302+BoazRosenberg@users.noreply.github.com>
2024-09-10 14:20:48 +0300

    update lessons and add images

--- datasets/stroop + Qs/prepare Qs.R
+++ datasets/stroop + Qs/prepare Qs.R
@@ -1,15 +1,24 @@
 library(tidyverse)
-
-
 library(readxl)
+
+
+#### prepare stroop data #####
+
 stroop_data <- read_excel("inst/tutorials/datasets/stroop + Qs/stroop_data.xlsx")
 
 stroop_sessions = stroop_data %>% select(session_id) %>% distinct() %>% pull(session_id)
 
 stroop_summ = stroop_data %>% group_by(session_id) %>%
-  summarise(nred = sum(color == "red"))
+  summarise(correct_percent = mean(correct,na.rm = T),
+            RT_congruent = mean(RT,Congruent))
 
-Qs <- read_csv("inst/tutorials/datasets/stroop + Qs/Qs .csv")
+
+
+
+
+
+
+Qs <- read_csv("inst/tutorials/datasets/stroop + Qs/Qs edited.csv")
 
 nfc = Qs %>% filter(!is.na(session_id)) %>%
    mutate(attention_check = ifelse(grepl("read",attentioncorrect),1,0)  ,
@@ -22,6 +31,6 @@ nfc = Qs %>% filter(!is.na(session_id)) %>%
 
 write.csv(nfc, "datasets/stroop + Qs/need_for_cognition.csv")
 
-usethis::use_data(nfc, overwrite = TRUE)
+usethis::use_data(stroop_data, overwrite = TRUE)
 
 joined = left_join(nfc,stroop_summ, by ="session_id")
